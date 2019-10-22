b8c8b61645894bcbe06aa7e073f80f5542fe2706
GilbertoJun <15beto15@gmail.com>
2019-10-22 03:19:01 -0300

    Visualizacao de veiculo

--- src/app/tela-inicial/tela-inicial.component.ts
+++ src/app/tela-inicial/tela-inicial.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'tela-inicial-app',
-    templateUrl: './tela-inicial.component.html'
+    templateUrl: './tela-inicial.component.html',
+    styleUrls: ['./tela-inicial.component.css']
 })
 
 export class TelaInicialComponent implements OnInit {
